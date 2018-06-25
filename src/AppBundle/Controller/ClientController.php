7070e294018cd860049035e05e94fdea8ae56c58
julien_ozae <julien@ozae.com>
2018-06-25 15:51:07 +0200

    route client updated, index deleted

--- src/AppBundle/Controller/ClientController.php
+++ src/AppBundle/Controller/ClientController.php
@@ -12,8 +12,8 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 class ClientController extends Controller
 {
     /**
-     * @Route("/client", name="client")
-     * @Method({"GET", "POST"})
+     * @Route("/client", name="getclient")
+     * @Method({"GET"})
      */
     public function getClientInfo(Request $request)
     {
@@ -23,23 +23,61 @@ class ClientController extends Controller
 		// Instantiate a Manager client (version 1.0 here)
 		$keyyo_manager = new \Keyyo\Manager\Client('1.0', $access_token);
 
-		// Retrieve all services from the authenticated customer
-		$services = $keyyo_manager->services(33175430270);
-        var_dump($services->name);
-        die();
+		// // Retrieve all services from the authenticated customer
+		$services = $keyyo_manager->services();
 
 		$profiles = [];
 		$i = 0;
 		foreach ($services as $service) {
-            var_dump($service);
-            die();
-    		$profiles[$i]["csi"]                = $service->csi;
-			$profiles[$i]["name"]               = $service->name;
+    		$profiles[$i]["csi"] = $service->csi;
+			$profiles[$i]["name"] = $service->name;
 			$i++;
 		}
-		var_dump($profiles);
-		die();
 
+        return $this->render('client.html.twig', [
+            'profiles' => $profiles,
+        ]);
+    }
+
+
+
+        /**
+     * @Route("/client", name="updateclient")
+     * @Method({"POST"})
+     */
+    public function postClientInfo(Request $request)
+    {
+        // included for test
+        require_once __DIR__ . '/../config.php';
+
+        var_dump(count($_POST) == 0);
+        var_dump(count($_GET) == 0);
+
+
+        if ($_POST["name"] != "") {
+
+        };
+
+        var_dump($services->name);
+
+        $services->__set('name','Pierrot');
+
+        var_dump($services->name);
+
+
+        // Instantiate a Manager client (version 1.0 here)
+        $keyyo_manager = new \Keyyo\Manager\Client('1.0', $access_token);
+
+        // // Retrieve all services from the authenticated customer
+        $services = $keyyo_manager->services();
+
+        $profiles = [];
+        $i = 0;
+        foreach ($services as $service) {
+            $profiles[$i]["csi"] = $service->csi;
+            $profiles[$i]["name"] = $service->name;
+            $i++;
+        }
 
         return $this->render('client.html.twig', [
             'profiles' => $profiles,
--- src/AppBundle/config.php
+++ src/AppBundle/config.php
@@ -5,5 +5,5 @@ $client_secret 	= '44001c168b7fefdcc2b8420d';
 $redirect_uri  	= 'http://localhost:8000/client';
 $code 			= 'f57106adba79fd7d608fb13b74f1eab5b975c48f';	
 $state 			= '5b29cea4ea38f';
-$access_token 	= 'HY3LDoIwFET/pVtZlFt5mbholERRQ+SxEDWmRRB8gNpWMMZ/t3E3M+ck80FHJhkaoe0OWRxskw+dgjNwdsjQiwuYkH8s1fV6YHleCKG7aYFnuZ6DXWMIBEwDtJIrIdtb8dS80fYeGUjUp4ZJ9Sz0w8pPJ3Of4qpXbTYIMSXq8mp6JyVh+YazWy4TNn9VPI18uYltoLVP20AOHo/LNE7W/SIW8SxIvDTrFgGmthUd77SLQpm0AvNujL4/';
+$access_token 	= 'HY1LDoIwAETv0jWJtlCg7hTREEVEjAhiTPkIgqDhY1ONd7dxNzPvJfMBKe0pmIBTBHCMVBgrWhZTpEVAEouOxrL8j9fhfr/QJMm6TnSIEcFEJTqRFCQjKCGhJEPXP+qsFbwR9hlIoLvlDe2HNhMPthkYlmtw/tqapAyn5dN28z1V5u0L57vjzGFVai+jUevjYLMwgqZnVjFdmfXi7S4VN7VqxtewhEV1uNHKs1m4VRkbkModz9H4xgffHw==';
 $refresh_token	= '7421e2f4486e4ddb6b02014bf9af25511954c59a';
